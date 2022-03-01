5e9c8e7fb8fe18ba4ac01368b51f1b6114acbd5b
dtheran001 <dannytheran29@gmail.com>
2022-03-02 00:12:00 +0100

    Añadiendo comunicación con la API de azure, a falta de la clave de activación

--- Software/alert_generator/main.py
+++ Software/alert_generator/main.py
@@ -1,8 +1,46 @@
+import http.client
+import urllib.error
+import urllib.parse
+import urllib.request
 from threading import Thread
 
 
 def process_video(id_camera) -> None:
-    pass
+    headers, params = url_define()
+
+    try:
+        conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/face/v1.0/detect?%s" % params, "{body}", headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+        conn.close()
+    except OSError as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
+def url_define():
+    attributes = 'mask,gender'
+    subscription_key = 'key'
+
+    headers = {
+        # Request headers
+        'Content-Type': 'application/jason',
+        'Ocp-Apim-Subscription-Key': subscription_key,
+    }
+
+    params = urllib.parse.urlencode({
+        # Request parameters
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': attributes,
+        'recognitionModel': 'recognition_04',
+        'returnRecognitionModel': 'false',
+        'detectionModel': 'detection_03',
+        'faceIdTimeToLive': '86400',
+    })
+
+    return [headers, params]
 
 
 def main() -> None:
