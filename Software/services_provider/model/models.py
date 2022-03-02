ca6df108a89224cbaeb4fc614c5c74973e70bc60
ikerh <ikerhabib@gmail.com>
2022-03-03 00:11:41 +0100

    Added services_provider model

---
+++ Software/services_provider/model/models.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from Software.services_provider import db
+
+
+class Alert(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    type: str = db.Column(db.String(256))
+    timestamp: int = db.Column(db.Integer)
+    resolved: bool = db.Column(db.Boolean)
+    camera: int = db.Column(db.Integer, db.ForeignKey('camera.id'))
+
+
+class Camera(db.Model):
+    id: int = db.Column(db.Integer, primary_key=True)
+    alias: str = db.Column(db.String(256))
+    active: bool = db.Column(db.Boolean)
+    alerts: List[Alert] = db.relationship('Alert')
