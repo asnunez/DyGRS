8835f16642ff3b7d90da2a2027101cea1135dce1
Iker-Costa <iker.costa@nemergent-solutions.com>
2022-03-15 15:19:32 +0100

    Updates

--- sw/camera_discoverer/tools.py
+++ sw/camera_discoverer/tools.py
@@ -1,31 +1,27 @@
 import json
+from typing import Dict
+
 import requests
 
 
-def create_json(ip: str, state: bool, number: int) -> json:
-    if state:
-        camera_info = {
-            "camera":
-                {
-                    "alias": "camera-%d" % number,
-                    "url": "rtsp://%s:554" % ip,
-                    "active": "True"
-                }
-        }
-    else:
-        camera_info = {
-            "camera":
-                {
-                    "alias": "camera-%d" % number,
-                    "url": "rtsp://%s:554" % ip,
-                    "active": "False"
-                }
-        }
-
-    return json.dumps(camera_info)
+def bool2str(b: bool) -> str:
+    return "True" if b else "False"
 
 
-def send_discover(ip: str, state: bool, number: int) -> None:
-    camera_info = create_json(ip, state, number)
-    requests.post("http://10.254.14.117:80/register-camera", json=camera_info)
+def create_json(host: str, alias: str, state: bool) -> Dict:
+    camera_info = {
+        "camera":
+            {
+                "alias": alias,
+                "url": f"rtsp://{host}",
+                "active": bool2str(state)
+            }
+    }
+
+    return camera_info
+
+
+def send_discover(host: str, alias: str, state: bool) -> None:
+    camera_info = create_json(host, alias, state)
+    requests.post("http://10.109.76.15:5000/register-camera", json=camera_info)
     print(camera_info)
