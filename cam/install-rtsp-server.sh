ce749c682f23ba2f34abc4bcea2e793de3febfd9
Iker-Costa <iker.costa@nemergent-solutions.com>
2022-04-28 15:28:02 +0200

    Cam

---
+++ cam/install-rtsp-server.sh
@@ -0,0 +1,3 @@
+wget https://github.com/aler9/rtsp-simple-server/releases/download/v0.18.0/rtsp-simple-server_v0.18.0_linux_amd64.tar.gz
+tar xvzf rtsp-simple-server_v0.18.0_linux_amd64.tar.gz
+mv rtsp-simple-server_v0.18.0_linux_amd64 camara
