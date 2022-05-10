c876fed37843da9bd16bec42971a1185325cbcee
dtheran001 <64919045+dtheran001@users.noreply.github.com>
2022-05-10 16:45:07 +0200

    Update start_cam.sh

--- cam/start_cam.sh
+++ cam/start_cam.sh
@@ -10,5 +10,5 @@ mkdir camera-dir
 tar -xvzf rtsp-simple-server_v0.18.0_linux_amd64.tar.gz -C camera-dir/
 cd camera-dir
 docker run --rm -d -v $PWD/rtsp-simple-server.yml:/rtsp-simple-server.yml -p 8554:8554 aler9/rtsp-simple-server
-sudo nohup ffmpeg -nostdin -framerate 24 -video_size 480x480 -i /cam.mp4 -f rtsp -rtsp_transport tcp rtsp://$IP:8554/cam &
+sudo nohup ffmpeg -nostdin -framerate 24 -video_size 480x480 -i /dev/media0 -f rtsp -rtsp_transport tcp rtsp://$IP:8554/cam &
 sudo avahi-publish-address -s $CAMERA_ALIAS _rtsp._tcp 8554 &
